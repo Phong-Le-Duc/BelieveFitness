7bede05a56341ff3f5a818f50108356507af15ca
Phong-Le-Duc <phongleduc@gmail.com>
2026-06-02 13:51:18 +0200

    feat: constrain site to mobile frame

--- src/app/SplashLayout.js
+++ src/app/SplashLayout.js
@@ -7,14 +7,16 @@ export default function SplashLayout({ children, fontClass }) {
     const [splashDone, setSplashDone] = useState(false);
 
     return (
-        <div className={fontClass}>
-            <SplashScreen onFinish={() => setSplashDone(true)} />
-            {splashDone && (
-                <>
-                    <Header />
-                    {children}
-                </>
-            )}
+        <div className="site-shell">
+            <div className={`${fontClass} app-shell`}>
+                <SplashScreen onFinish={() => setSplashDone(true)} />
+                {splashDone && (
+                    <>
+                        <Header />
+                        {children}
+                    </>
+                )}
+            </div>
         </div>
     );
 }
